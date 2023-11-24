706daa778bd19600f5b3d988105b495373881f5f
Anthony Cyrille <ancyr@pm.me>
2023-11-24 05:26:46 +0100

    task: subarrays

--- src/subarrays_1343/Solution.kt
+++ src/subarrays_1343/Solution.kt
@@ -1,3 +1,23 @@
 package subarrays_1343
 
-: sub
+class Solution {
+  fun numOfSubarrays(arr: IntArray, k: Int, threshold: Int): Int {
+    var count = 0;
+    var accumulator = 0;
+    var left = 0;
+
+    for (right in 0 until arr.size) {
+      accumulator += arr[right];
+      if (right - left + 1 >= k) {
+        if (accumulator / k >= threshold) {
+          count++;
+        }
+
+        accumulator -= arr[left];
+        left++;
+      }
+    }
+
+    return count;
+  }
+}

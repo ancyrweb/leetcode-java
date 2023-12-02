dfee3a9f047de7c95dd37cbae0dab914f1f6bcb2
Anthony Cyrille <ancyr@pm.me>
2023-12-02 04:11:28 +0100

    task: graph valid tree finally fixed

--- src/graph_valid_tree_261/Solution.kt
+++ src/graph_valid_tree_261/Solution.kt
@@ -2,56 +2,34 @@ package graph_valid_tree_261
 
 class Solution {
   fun validTree(n: Int, edges: Array<IntArray>): Boolean {
-    if (n == 1) {
-      return true
-    }
-
     val visited = HashSet<Int>()
-    val childrens = HashSet<Int>()
-    val adj = HashMap<Int, MutableList<Int>>()
-
-    for (i in 0 until n) {
-      adj[i] = mutableListOf<Int>()
-    }
+    val adj = Array<MutableList<Int>>(n) { _ -> mutableListOf() }
 
     for (edge in edges) {
-      adj[edge[0]]!!.add(edge[1])
-      adj[edge[1]]!!.add(edge[0])
+      adj[edge[0]].add(edge[1])
+      adj[edge[1]].add(edge[0])
     }
 
-    var parent = -1
-    for (i in 0 until n) {
-      if (!childrens.contains(i)) {
-        if (parent != -1) {
-          return false
+    fun hasCycle(node: Int, parent: Int): Boolean {
+      if (visited.contains(node)) {
+        return true
+      }
+
+      visited.add(node)
+
+      for (neighbor in adj[node]) {
+        if (neighbor == parent) {
+          continue
         }
 
-        parent = i
+        if (hasCycle(neighbor, node)) {
+          return true
+        }
       }
-    }
 
-    if (parent == -1) {
       return false
     }
 
-    val q = mutableListOf<Int>(parent)
-    var prev: Int? = null
-    while (q.isNotEmpty()) {
-      for (i in 0 until q.size) {
-        val next = q.removeFirst()
-        if (visited.contains(next)) {
-          return false
-        }
-
-        visited.add(next)
-        for (a in adj[next]!!) {
-          if (a != next) {
-            q.add(a)
-          }
-        }
-      }
-    }
-
-    return visited.size == n
+    return !hasCycle(0, -1) && visited.size == n
   }
 }

b5e7caa46b29f90fe9ab3f2ecfe61b543a2493c5
Anthony Cyrille <ancyr@pm.me>
2023-12-06 10:26:20 +0100

    task: started with rabin karp

--- src/find_index_first_occurence_28/Solution.kt
+++ src/find_index_first_occurence_28/Solution.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class Solution {
-  fun strStr(haystack: String, needle: String): Int {
+  fun bruteforce(haystack: String, needle: String): Int {
     if (needle.length > haystack.length) {
       return -1
     }
@@ -28,6 +28,27 @@ class Solution {
     }
 
     return -1
+  }
+
+  fun strStr(haystack: String, needle: String): Int {
+    // Rabin Karp
+    fun hash(s: String): Int {
+      val radix = 26 // alphabet
+      val mod = 1000000033 // prime
+      var hashp: Int = 0
+      for (i in s) {
+        hashp = (radix * hashp + i.code) % mod
+      }
+
+      return hashp
+    }
+
+    val needleHash = hash(needle)
+    var m = needle.length
+    var n = haystack.length
+
+
+    return 0
   }
 
   @Test

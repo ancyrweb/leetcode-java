c9a5309b5468f44a889f48b3b28cb8873f2e2dd1
Anthony Cyrille <12291995+ancyrweb@users.noreply.github.com>
2026-03-12 07:49:40 +0100

    task: valid anagram

---
+++ src/leetcode_2025/valid_anagram_242/Solution.kt
@@ -0,0 +1,43 @@
+package leetcode_2025.valid_anagram_242
+
+class Solution {
+  fun isAnagram(s: String, t: String): Boolean {
+    val sChars = s.toCharArray().also { it.sort() }
+    val tChars = t.toCharArray().also { it.sort() }
+    return sChars.contentEquals(tChars)
+  }
+
+  fun isAnagram_maps(s: String, t: String): Boolean {
+    if (s.length != t.length) {
+      return false
+    }
+
+    val aMap = mutableMapOf<Char, Int>()
+    for (c in s) {
+      aMap[c] = aMap.getOrDefault(c, 0) + 1
+    }
+
+    val bMap = mutableMapOf<Char, Int>()
+    for (c in t) {
+      bMap[c] = bMap.getOrDefault(c, 0) + 1
+    }
+
+    for (c in aMap.keys) {
+      if (aMap[c] != bMap[c]) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  fun isAnagram_sort(s: String, t: String): Boolean {
+    if (s.length != t.length) {
+      return false
+    }
+
+    val sChars = s.toCharArray().also { it.sort() }
+    val tChars = t.toCharArray().also { it.sort() }
+    return sChars.contentEquals(tChars)
+  }
+}
---
+++ src/leetcode_2025/valid_anagram_242/SolutionTest.kt
@@ -0,0 +1,22 @@
+package leetcode_2025.valid_anagram_242
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class SolutionTest {
+  fun run(s: String, t: String): Boolean {
+    val solution = Solution()
+    return solution.isAnagram(s, t)
+  }
+
+  @Test
+  fun case1() {
+    assertTrue(run("anagram", "nagaram"))
+  }
+
+  @Test
+  fun case2() {
+    assertFalse(run("rat", "car"))
+  }
+}

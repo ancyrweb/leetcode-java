728d17b4b74dda9bcb9589da521aae340c0fb602
Anthony Cyrille <12291995+ancyrweb@users.noreply.github.com>
2026-03-12 08:38:22 +0100

    task: group anagram

---
+++ src/leetcode_2025/group_anagram_49/Solution.kt
@@ -0,0 +1,17 @@
+package leetcode_2025.group_anagram_49
+
+class Solution {
+  fun groupAnagrams(strs: Array<String>): List<List<String>> {
+    val maps = mutableMapOf<String, MutableList<String>>()
+    for (s in strs) {
+      val key = s.toCharArray().sorted().toString();
+      if (maps.containsKey(key)) {
+        maps[key]?.add(s)
+      } else {
+        maps[key] = mutableListOf(s)
+      }
+    }
+
+    return maps.values.toList()
+  }
+}
---
+++ src/leetcode_2025/group_anagram_49/SolutionTest.kt
@@ -0,0 +1,25 @@
+package leetcode_2025.group_anagram_49
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SolutionTest {
+  fun run(strs: Array<String>): List<List<String>> {
+    val solution = Solution()
+    return solution.groupAnagrams(strs)
+  }
+
+  @Test
+  fun case1() {
+    assertEquals(
+      listOf(
+        listOf("eat", "tea", "ate"),
+        listOf("tan", "nat"),
+        listOf("bat"),
+      ),
+      run(
+        arrayOf("eat", "tea", "tan", "ate", "nat", "bat")
+      ),
+    )
+  }
+}
